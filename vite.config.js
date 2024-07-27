c510a93ec09452e973bebd5b9dadf3b35260fd58
alexandre artus <artusalexandre74@gmail.com>
2024-07-27 17:07:41 +0200

    test with other vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/alexandre-portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/alexandre-portfolio/' : '/',
   build: {
     outDir: 'dist'
   }
