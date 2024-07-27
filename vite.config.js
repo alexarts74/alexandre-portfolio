58b508ce1e414c6919d7ad7a56e2906aa7a56362
alexandre artus <artusalexandre74@gmail.com>
2024-07-27 20:25:07 +0200

    merci yanis

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,10 @@ import { defineConfig } from 'vite'
 
  export default defineConfig({
     base: '/alexandre-portfolio/',
+    resolve: {
+        alias: {
+          '@': '/src',
+          '@firecms/neat': '/node_modules/@firecms/neat'
+        }
+      }
   })
