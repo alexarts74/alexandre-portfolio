470d5ad1ee4d94c8f09dce7ad7f0bf53ead6a24b
alexandre artus <artusalexandre74@gmail.com>
2024-07-27 21:04:43 +0200

    merci yanis

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
 import { defineConfig } from 'vite'
 
  export default defineConfig({
-   // base: '/alexandre-portfolio/',
-    build: {
-        outDir: 'dist', // Ensure this matches your workflow config
-      },
-      resolve: {
-        alias: {
-          '@': '/src',
-          '@firecms/neat': '/node_modules/@firecms/neat',
-        },
-      },
+    base: '/alexandre-portfolio/',
   })
-//test
